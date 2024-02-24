bd7aca9ed959ff03b3fa826cf6a530bebbd3c437
tian <tian@ubuntu22.myguest.virtualbox.org>
2024-02-23 20:40:35 -0500

    computer vision submit, add comments

--- robotics_A2/arrow_tracker.py
+++ robotics_A2/arrow_tracker.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import numpy as np
 import ballTracker
@@ -16,6 +18,7 @@ tracker_type = TRACKER_BALL
 
 
 def preprocess_arrow_template(image_path):
+    # load all the 3 types of arrow pictures used for shape matching.
     gray_frame = cv2.resize(cv2.imread(image_path, cv2.IMREAD_GRAYSCALE), (640, 480))
     template_contours, _ = cv2.findContours(gray_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     template_contour = None
@@ -32,49 +35,72 @@ invalid_score = 0
 
 
 def get_canny(frame):
-    # low_threshold = cv2.getTrackbarPos('Low Threshold', 'Canny Parameters')
-    # high_threshold = cv2.getTrackbarPos('High Threshold', 'Canny Parameters')
     edges = cv2.Canny(frame, 50, 150)
     return edges
 
-
+# TrackArrows function used to track the arrow target and identify the diretion.
 def trackArrows(frame):
-    match_threshold = 0.4  # This threshold value is adjusted during debugging
+
+    #time_start = time.perf_counter() # Record the starting time
+
+    # STEP1: PREPROCESS IMAGES.
+    # This threshold value is adjusted during debugging. This is the maximum values for shape matching
+    match_threshold = 0.4
     resized_frame = cv2.resize(frame, (640, 480))
     gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_RGB2GRAY)
+    # Use gray formatting to get the edges of the camera, and then find contours.
     edges = get_canny(gray_frame)
+    # Use the cv2.RETR_CCOMP to ensure the internal edges are observed as well.
     contours, _ = cv2.findContours(edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    # Record the contours with the max areas, the minimum match scores
     max_area = 0
     max_contour = None
     arrow_type = INVALID_ARROW
     best_approx = 0
+    min_match_score = 100
+    #time_preprocess = time.perf_counter()
+    #print(f"time preprocessing:{(time_preprocess - time_start) * 1000}")
+
+    # STEP2: FILTERING CONTOURS USING SHAPE MATCHING AND POLYGON APPROXIMATION.
     for contour in contours:
         epsilon = 0.02 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
         area = cv2.contourArea(contour)
+        # ignore the areas smaller than 300. This threshold value is adjusted through debugging
         if area < 300:
             continue
+        # Only focus on the 7-edged and 9-edged polygons which are the forward arrow and turning arrows respectively.
         if len(approx) == 7 or len(approx) == 9:
             forward_match = cv2.matchShapes(forward_contour, contour, cv2.CONTOURS_MATCH_I1, 0.0)
             left_match = cv2.matchShapes(left_contour, contour, cv2.CONTOURS_MATCH_I1, 0.0)
             right_match = cv2.matchShapes(right_contour, contour, cv2.CONTOURS_MATCH_I1, 0.0)
             min_match = min(forward_match, left_match, right_match)
-            if min_match > match_threshold:
+            # find the minimum match scores and compare it with the previous minimum one and the threshold.
+            if min_match > match_threshold or min_match > min_match_score:
                 continue
+            # Meanwhile, find the maximum area. This 2 conditions combined are sometimes too strict and can cause problems.
+            # So it's also feasible to skip one of these them.
             if max_area > area:
                 continue
             else:
+                # record all the optimal values.
+                min_match_score = min_match
                 max_contour = contour
                 max_area = area
                 best_approx = approx
             if len(approx) == 7:
+                # the 7-edge polygon is technically the forward arrow.
                 arrow_type = FORWARD_ARROW
+    #arrow_identify_time_start = time.perf_counter()
 
+    # STEP3: IDENTIFY THE ARROW DIRECTION BASED ON ANGLES OF MINIMUM AREA RECTANGLE
     if max_contour is not None:
         angle = 90
         rect = cv2.minAreaRect(best_approx)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
+        # Calculate the minimum area rectangle and get the angle of it.
+        # Decide the direction of the arrow base on the angle of the rectangle.
         if arrow_type != FORWARD_ARROW:
             _, _, angle = rect
             if angle > 60:
@@ -88,9 +114,14 @@ def trackArrows(frame):
             print(f"angle = {angle}, max_area = {max_area}")
             print(f"suspecting arrow type is : {arrow_type}, number of edges = {len(best_approx)}")
             cv2.drawContours(gray_frame, [max_contour], -1, (0, 255, 0), 3)
+    # THe following is to record the time expenses.
+    #arrow_identify_time_end = time.perf_counter()
+    #print(f"time for arrow identifying:{(arrow_identify_time_end - arrow_identify_time_start) * 1000}")
+    #print(f"time for arrow targeting:{(arrow_identify_time_end - time_preprocess) * 1000}")
     return gray_frame, edges, arrow_type
 
-
+# The results may change quickly because sometimes it's affected by noises and mistakes.
+# So a scoring system is added here to keep the results steady
 def analyze_arrow_tracking_res(arrow_type):
     image = None
     global invalid_score
@@ -99,10 +130,13 @@ def analyze_arrow_tracking_res(arrow_type):
     else:
         result_type.append(arrow_type)
     if invalid_score == 30:
+        # If the invalid results hit 30 which means currently
+        # there's no valid target detected, so clear all the previous records
         invalid_score = 0
         result_type.clear()
         cv2.destroyWindow("result")
     if len(result_type) == 10:
+        # Calculate the average score of 10 results and keep the most frequent result.
         mean_val = np.mean(result_type)
         print(f"mean value {mean_val} of arrow_type = {arrow_type}")
         invalid_score = 0
@@ -122,11 +156,13 @@ def openCam():
         ret, frame = cap.read()
         if not ret:
             break
-        # low_threshold = cv2.getTrackbarPos('Low Threshold', 'Canny Parameters')
-        ballTracker.trackBall(frame)
+        # Start Ball tracking, this will not be commented for tracking balls.
+        #ballTracker.trackBall(frame)
+
+        # Start Arrow tracking.
         gray_result, edges, arrow_type = trackArrows(frame)
-        #top_row = cv2.hconcat(([gray_result, edges]))
-        #cv2.imshow("debug", top_row)
+        top_row = cv2.hconcat(([gray_result, edges]))
+        cv2.imshow("debug", top_row)
         analyze_arrow_tracking_res(arrow_type)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -134,10 +170,5 @@ def openCam():
     cap.release()
     cv2.destroyAllWindows()
 
+openCam()
 
-def main():
-    openCam()
-
-
-if __name__ == "__main__":
-    main()
--- robotics_A2/ballTracker.py
+++ robotics_A2/ballTracker.py
@@ -1,27 +1,38 @@
 
 import cv2
 import numpy as np
+import time
 def preprocess_cam_hsv(frame):
-    hsv_lower = np.array([0, 58, 232])  # Default lower HSV value
-    hsv_higher = np.array([18, 206, 255])  # Default upper HSV value
-    # Resize the frame
+    # Preprocess the frames and convert them to HSV color spaces
+    # The lower and higher thresholds are all adjusted through debugging
+    hsv_lower = np.array([0, 96, 215])  # Default lower HSV value
+    hsv_higher = np.array([18, 255, 255])  # Default upper HSV value
+    # 1. Resize the frame
     current_frame = cv2.resize(frame, (640, 480))
-    # 1. Color Extraction
+    # 2. Color Extraction
     # Convert to HSV and apply mask
     color_hsv_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
-    # Blurring and edge detection
+    # 3. Blurring and edge detection and obtain the mask
     blurred = cv2.GaussianBlur(color_hsv_frame, (5, 5), 0)
     mask = cv2.inRange(blurred, hsv_lower, hsv_higher)
     return current_frame, mask
 
 
 def trackBall_ApproxPolyDP(frame):
+    #time_pre_approx = time.perf_counter()
+    # 1. resize the frame
     current_frame = cv2.resize(frame, (640, 480))
+    # 2. obtain the gray formatting frame
     gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+    # 3. blur the frame
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # 4. get the edges through canny
     edges = cv2.Canny(blurred, 50, 150)
+    #time_pre_approx_end = time.perf_counter()
+    #print(f"time approx_preprocessing:{(time_pre_approx_end - time_pre_approx) * 1000}")
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     circles = []
+    # 5. filter the contours using polygon approximating and circularity calculation.
     for contour in contours:
         epsilon = 0.02 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
@@ -30,21 +41,31 @@ def trackBall_ApproxPolyDP(frame):
             perimeter = cv2.arcLength(contour, True)
             if perimeter == 0:
                 continue
+            # Calculate the circularity and determine if it's a circle.
             circularity = 4 * np.pi * area / (perimeter * perimeter)
             if area > 100 and circularity > 0.4:
                 circles.append(contour)
                 cv2.drawContours(current_frame, [approx], -1, (0, 255, 0), 2)
+    #time_approx_end = time.perf_counter()
+    #print(f"time approx_approximating:{(time_approx_end - time_pre_approx_end) * 1000}")
+    #print(f"time approx_processing:{(time_approx_end - time_pre_approx) * 1000}")
     return circles, current_frame, edges
 
 
 def trackBall_Color_extraction(frame):
+    #time_pre_start = time.perf_counter()
+    # S1. Obtain the mask through HSV preprocessing
     current_frame, mask = preprocess_cam_hsv(frame)
+    #time_pre_end = time.perf_counter()
+    #print(f"time color_preprocessing:{(time_pre_end - time_pre_start) * 1000}")
+
     # Morphological opening, closing
     kernel = np.ones((5, 5), np.uint8)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    # S2. let the frame pass through the closing morphology and find contours
     closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     contours, _ = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     circles = []
+    # Calculate circularity and get the maximum areas as the ball.
     for contour in contours:
         area = cv2.contourArea(contour)
         perimeter = cv2.arcLength(contour, True)
@@ -53,10 +74,15 @@ def trackBall_Color_extraction(frame):
         circularity = 4 * np.pi * area / (perimeter * perimeter)
         if area > 100 and circularity > 0.4:
             circles.append(contour)
+            #cv2.drawContours(current_frame, [contour], -1, (0, 255, 0), 2)
+    #time_color_end = time.perf_counter()
+    #print(f"time color_extraction:{(time_color_end - time_pre_end) * 1000}")
+    #print(f"time color_processing:{(time_color_end - time_pre_start) * 1000}")
     return circles, current_frame, closing
 
 def analyze_result(circles, current_frame, method):
     if circles:
+        # Calculate the positions and draw the rectangle of the circle.
         max_contour = max(circles, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(max_contour)
         points_left = (x - 5, y - 5)
